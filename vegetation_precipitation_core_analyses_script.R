6c6aaefd22ce84290fcf76842e91a394d2e2dcea
Andrew Felton <A02296270@aggies.usu.edu>
2019-05-13 14:33:59 -0600

    small update

--- vegetation_precipitation_core_analyses_script.R
+++ vegetation_precipitation_core_analyses_script.R
@@ -1,29 +1,31 @@
 #script to repeat
-test.strat<-stratified(stratified_df, c("region","map"), 0.1)
-#test.strat.raster<-rasterFromXYZ(test.strat)
-#plot(test.strat.raster)
+test.strat.northern_mixed<-stratified(northern_mixed_prairies_above_below, c("map"), 0.01)
+test.strat.cold_deserts<-stratified(cold_deserts_above_below, c("map"), 0.01)
+test.strat.california_annuals<-stratified(california_annuals_above_below, c("map"), 0.05)
+test.strat.semiarid_steppe<-stratified(semiarid_steppe_above_below, c("map"), 0.02)
+test.strat.hot_deserts<-stratified(hot_deserts_above_below, c("map"), 0.02)
+
+test.strat<-rbind(test.strat.northern_mixed, test.strat.cold_deserts, test.strat.california_annuals, 
+                  test.strat.semiarid_steppe, test.strat.hot_deserts)
 #head(test.strat)
 #summary(test.strat)
+#test.strat.visualize = subset(test.strat, select = -c(region,map) )
+#test.strat.raster<-rasterFromXYZ(test.strat.visualize)
+#plot(test.strat.raster)
 
+#final dataframe to put into model
 stratified_final<-merge(test.strat, rangeland_npp_covariates_deviations_1,by=c('x','y'))
-#head(stratified_final)
 
-#get residuals of LM
-test.strat_lm<-lm(npp.x~mm.y*region.x*mm.dev
-                  ,stratified_final)
-#summary(test.strat_lm)
-stratified_final$resids<-resid(test.strat_lm)
-
-#make a spatial neighborhood weights list
+#make a distance-based spatial neighborhood weights list
 xy_stratified<-cbind(stratified_final$x,stratified_final$y)
 wd5_xy_stratified <- dnearneigh(xy_stratified, 0, 5) #range of 5 degrees
 W_xy_stratified <- as(nb2listw(wd5_xy_stratified, style="W", zero.policy=TRUE), "CsparseMatrix") #create a sparse weights matrix
 sparse_neighbor_weights_xy_stratified <- mat2listw(W_xy_stratified)
-#moran.mc(stratified_final$resids, sparse_neighbor_weights_xy_stratified, nsim=999)
-lag_full <- lagsarlm(npp.x~mm.y*region.x*mm.dev,data=stratified_final, listw = sparse_neighbor_weights_xy_stratified)
-#summary(lag_full) 
-coefs<-lag_full$coefficients
-coefs.df<-data.frame(coefs)
 
-#code to export this to a folder....
-
+#spatial error model
+stratified_error<-errorsarlm(npp.x~mm.y*region.x*mm.dev,data=stratified_final, listw = sparse_neighbor_weights_xy_stratified, tol.solve = 1e-30)
+summary(stratified_error)
+system.time(stratified_error)
+#put coefficients into dataframe
+coefs_spatial_error<-test_error_model$coefficients
+coefs_spatial_error_df<-data.frame(coefs_spatial_error)
