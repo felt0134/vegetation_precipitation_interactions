5fdd693e748e83328deeece14da9eeb596bfc2fa
Andrew Felton <A02296270@aggies.usu.edu>
2019-12-13 16:54:08 -0700

    coding for making good projections for rasters, still need to clean this up and remove excess code.

--- extra.R
+++ extra.R
@@ -45,13 +45,51 @@ summary(hot_deserts_dry_map)
 library(raster)
 library(sp)
 library(rgdal)
+#change projection
+projInfo(type = "proj")
+?projInfo
+test<-projectExtent(crop_mm_raster, target_CRS)
+proj4string(crop_mm_raster) <- CRS("+proj=longlat")
+aea.proj <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96+x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
+raster_AES <- projectRaster(crop_mm_raster, crs=aea.proj) 
+shapefile_aes<-spTransform(states_all_sites,aea.proj)
+plot(raster_AES)
+plot(shapefile_aes)
+crop_extent<-zoom()
+crop_test_2<-crop(raster_AES,extent(crop_extent))
+plot(crop_test_2)
+target_CRS <- "+proj=utm +zone=12 +north +ellps=WGS84 +units=m"        
+test_raster_2<- projectRaster(sensitivity_raster, test, crs=target_CRS,method="bilinear", 
+                                     alignOnly=FALSE, over=FALSE)  
+proj4string(sensitivity_raster_2)
+state_all_sites_2 <- spTransform(states_all_sites , CRS(target_CRS))
+proj4string(state_all_sites_2)
+plot(state_all_sites_2)
+plot(sensitivity_raster_2)
 
-proj4string(sensitivity_raster)<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
-spTransform(states_all_sites)<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
-plot(sensitivity_raster)
-For colors, there are a lot of ways to do it.  Here's what I'm currently doing:
+#Create a matrix with random data & use image()
+xy <- matrix(rnorm(400),20,20)
+image(xy)
+
+# Turn the matrix into a raster
+rast <- raster(xy)
+# Give it lat/lon coords for 36-37°E, 3-2°S
+extent(rast) <- c(-122, -97.4375, 29.1875, 49)
+
+# ... and assign a projection
+projection(crop_mm_raster) <- CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+plot(crop_mm_raster)
+plot(state_all_sites_2),add=TRUE)
+library(tmap)
+library(tmaptools)
+
+set_projection(crop_mm_raster, projection = aea.proj.2)
+plot(crop_mm_raster)
+crs(sensitivity_raster_2) <- CRS('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs')
+test_crop<-crop(state_all_sites_2,extent(crop_mm_raster))
+#For colors, there are a lot of ways to do it.  Here's what I'm currently doing:
 #  1) Identify vectors of colornames that will define how a palette progresses. Here are a few that I've generated:
-
+plot(test_crop)
   brown2green <- c("saddlebrown", "tan", "darkolivegreen1", "darkolivegreen")
 red2pink <- c("gray20", "saddlebrown", "tan1", "red3", "pink")
 yopr <- c( "navy", "lightskyblue1", "gray20",    "saddlebrown" ,"red", "pink","orange","yellow")
@@ -79,17 +117,19 @@ delbkcols <- c( colorRampPalette(red2pink)(sum(bks<0) ),colorRampPalette(blue2da
 precip= c("red", "orange", "yellow",'green','cyan3','purple')
 bkcols.precip <- colorRampPalette(precip)(length(bks_map)-1)
 bks_map<- quantile(mean_mm$mm.x, probs=seq(0, 1, by=0.05), na.rm = TRUE)
-proj4string(mean_mm_raster)<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+proj4string(mean_mm_raster)<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +datum=NAD83 +units=m +no_defs")
  
 r.range <- round(c(minValue(mean_mm_raster), maxValue(mean_mm_raster)))
 
-plot(crop_mm_raster,breaks = bks_map,axes=F,box=F,col = bkcols.precip,
+plot(crop_test_2,breaks = bks_map,axes=F,box=F,col = bkcols.precip,
      legend.width=2,
      axis.args=list(at=seq(r.range[1], r.range[2], 100),
                                        labels=seq(r.range[1], r.range[2], 100),
                                    cex.axis=0.6),
      legend.args=list(text='', side=1, font=2, line=2.5, cex=0.8))
-plot(states_all_sites, lwd = 1,add=TRUE) 
+plot(shapefile_aes, lwd = 1,add=TRUE)
+
+test_crop<-crop(shapefile_aes,extent(raster_AES))
 
 #mean primary production
 npp= c('wheat3','wheat', "orange", "yellow",'green','darkgreen')
@@ -99,39 +139,49 @@ proj4string(mean_production_raster)<-CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat
 
 r.range.npp <- round(c(minValue(mean_production_raster), maxValue(mean_production_raster)))
 
-plot(mean_production_raster,breaks = bks_npp,box=F,axes=F,col = bkcols.npp,
+#update projection
+proj4string(mean_production_raster) <- CRS("+proj=longlat")
+mean_production_raster_2<-projectRaster(mean_production_raster, crs=aea.proj)
+plot(mean_production_raster_2)
+mean_production_raster_3<-crop(mean_production_raster_2,extent(crop_extent))
+
+#plot it
+plot(mean_production_raster_3,breaks = bks_npp,box=F,axes=F,col = bkcols.npp,
      legend.width=2,
      axis.args=list(at=seq(r.range.npp[1], r.range.npp[2], 100),
                     labels=seq(r.range.npp[1], r.range.npp[2], 100),
                     cex.axis=0.6),
      legend.args=list(text='', side=1, font=2, line=2.5, cex=0.8))
-plot(states_all_sites, lwd = 1,add=TRUE) 
+plot(out, lwd = 1,add=TRUE) 
 
-plot(mean_production_raster)
+
 #sensitivity to precip
 sensitivity=c("purple",'cyan3','green','yellow','orange','red')
 bkcols.sensitivity <- colorRampPalette(sensitivity)(length(bks)-1)
 r.range.sens <- round(c(minValue(sensitivity_raster), maxValue(sensitivity_raster)),digits=2)
 
-plot(sensitivity_raster,breaks = bks,axes=F,box=F,col = bkcols.sensitivity,
-     legend.width=2,
+us.cropped<-crop(states_all_sites,extent(sensitivity_raster))
+plot(us.cropped)
+rast_new = projectExtent(sensitivity_raster)
+rast_new = projectRaster(sensitivity_raster)
+values(rast_new) = values(rast1)
+map("state",proj="conic",par=39.83,add=TRUE)
+
+#update projection
+proj4string(sensitivity_raster) <- CRS("+proj=longlat")
+sensitivity_raster_2<-projectRaster(sensitivity_raster, crs=aea.proj)
+plot(mean_production_raster_2)
+sensitivity_raster_3<-crop(sensitivity_raster_2,extent(crop_extent))
+
+plot(sensitivity_raster_3,breaks = bks,axes=F,box=F,col = bkcols.sensitivity,legend=TRUE,
+     legend.width=1, legend.shrink=.75,
      axis.args=list(at=seq(r.range.sens[1], r.range.sens[2], 0.1),
                     labels=seq(r.range.sens[1], r.range.sens[2], 0.1),
                     cex.axis=0.6),
-     legend.args=list(text='', side=4, font=2, line=2.5, cex=0.8))
-plot(states_all_sites,lwd = 1,add=TRUE)
-box(lty = '1373', col = 'red')
-e<-extent(sensitivity_raster)
-test_crop<-crop(states_all_sites,e)
-plot(test_crop)
-states_all_sites
-par(mar=c(1,1,1,1))
-par(mar=c(0,0,0,0))
- plot(mean_mm_raster)
- 
-#
+     legend.args=list(text='', side=4, font=2, line=2.5, cex=15))
+plot(shapefile_aes,add=TRUE,lwd = 1)
 
-
+help(plot, package="raster")
 #For the isolines, I don't have a ready example, but I think this function might be what you need: https://stat.ethz.ch/R-manual/R-devel/library/MASS/html/kde2d.html
 
 
